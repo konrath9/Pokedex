90534b561c7f257408c5d15cef267d1b95230938
konrath9 <taynankonrath@gmail.com>
2023-08-03 21:45:55 -0300

    Adicionando rotas

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,13 +1,15 @@
 import React from "react"
-import { Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Home } from '../pages/Home';
 import { Home } from '../pages/Profile';
 
 export const name = (props) => {
   return (
-    <Routes>
+    <BrowserRouter>
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
-    </Routes>
+      </Routes>
+    </BrowserRouter>
   )
 }
